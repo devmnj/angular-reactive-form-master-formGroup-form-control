3cb3a8b2dd982ccde7ba406303954f87a9107e88
manojap <manojap@outlook.com>
2021-02-09 17:23:30 +0530

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
+import { FormControl, FormGroup,FormBuilder } from '@angular/forms';
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-root',
@@ -8,25 +11,51 @@ import { Component } from '@angular/core';
       <h1>
         Welcome to {{title}}!
       </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
+      {{todoForm.value | json}}<br/>
+      <button class="btn btn-primary" (click)="predefValue()">Fill default</button>
+      <div class="container">
+      <div class="aletr alert-danger" *ngIf="errorMsg">
+      {{errorMsg}}
+      </div>
+      <form [formGroup]="todoForm"   novalidation (ngSubmit)="OnSubmit()"  >
+       
+      <div class="form-group">
+      <label>Title</label>
+      <input type="text"  required   class="form-control" formControlName="item">
+
+      </div>
+      <div class="form-group ">
+      <label>Description</label>
+      <textarea class="form-control" minlength="10" formControlName="description" ></textarea>
+      </div>
+       <button  type="submit" class="btn btn-primary">Submit</button>
+      </form>
+      </div>
     </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
   `,
   styles: []
 })
 export class AppComponent {
   title = 'todo-form-master';
+  todoForm = new FormGroup({
+    item: new FormControl('MDT'),
+    description: new FormControl('')
+  });
+  public submitted = false;
+  todoModel = new Todo('New todo', 'some text');
+  constructor(private _todoService: TodoService) { }
+  errorMsg = '';
+
+  OnSubmit(): void {
+    this.submitted = true;
+    console.log(this.todoModel);
+    // tslint:disable-next-line: max-line-length
+    this._todoService.todoPost(this.todoModel).subscribe((data: any) => console.log('Success', data), (error: any) => this.errorMsg = error.statusText);
+  }
+  // setValue
+  // patchValue
+  predefValue(): void {
+    this.todoForm.patchValue({ item: 'MDF form' });
+  }
+
 }
